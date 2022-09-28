3b4675d6c7640c4489a95d256b413384e671e011
Brainybrian316 <brainybrian316@gmail.com>
2022-09-27 21:06:30 -0400

    autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+/* notes: 
+1. decorators take 3 arguments: target, propertyKey, descriptor
+2. target is the class prototype
+3. propertyKey is the name of the method
+4. descriptor is the property descriptor for the method
+*/
+// auto bind decorator
+function AutoBind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+	// descriptor.value is the original method implementation (the function)
+	const originalMethod = descriptor.value;
+	// adjustedDescriptor is the new property descriptor for the method
+	const adjustedDescriptor: PropertyDescriptor = {
+		// configurable is set to true so that the method can be overwritten
+		configurable: true,
+		// get is a getter function that returns a new function
+		get() {
+			// we bind the original method to the instance of the class.
+			const boundFn = originalMethod.bind(this);
+			// we return the bound function
+			return boundFn;
+		},
+	};
+	// we return the adjusted descriptor to overwrite the original descriptor
+	return adjustedDescriptor;
+}
+
 class ProjectInput {
 	// these are the fields of our class
 	templateElement: HTMLTemplateElement;
@@ -33,6 +59,7 @@ class ProjectInput {
 		this.attach();
 	}
 
+	@AutoBind
 	// binding the configure method to this method so that we can use 'this' in the configure method
 	private submitHandler(event: Event) {
 		event.preventDefault();
@@ -41,7 +68,7 @@ class ProjectInput {
 
 	// keeping a separation of concerns is why we are setting this up as private
 	private configure() {
-		this.element.addEventListener('submit', this.submitHandler.bind(this)); // bind the 'this' keyword to the class
+		this.element.addEventListener('submit', this.submitHandler);
 	}
 
 	private attach() {
