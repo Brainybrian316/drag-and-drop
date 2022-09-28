af46be395d2deb6cab51292da7b015b91febe564
Brainybrian316 <brainybrian316@gmail.com>
2022-09-27 20:35:09 -0400

    project input form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+class ProjectInput {
+	// these are the fields of our class
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+
+	constructor() {
+		// '!' tells typescript to be non-null
+		this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+		// 'as' is type casting to let typescript know this will be of 'element'
+		this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+		// importNode is a document property on the document object 'content' exist on HTML elements
+		const importedNode = document.importNode(this.templateElement.content, true); // takes 2 arguments
+		// points to the node element of the template element which is a form element in this case
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		// call attach method to render the form
+		this.attach();
+	}
+	private attach() {
+		// place to render content and get access to concrete HTML element stored in the 'this' property
+		this.hostElement.insertAdjacentElement('afterbegin', this.element);
+	}
+}
+
+const prjInput = new ProjectInput();
