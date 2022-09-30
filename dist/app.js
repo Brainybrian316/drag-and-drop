e106774556c24f876f520e0840c7acf84b9d15a9
Brainybrian316 <brainybrian316@gmail.com>
2022-09-30 18:57:22 -0400

    using ES Modules imports instead of references

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,6 @@
-// Modules in es2015 are loaded asynchronously
 import { ProjectInput } from './components/project-input.js';
 import { ProjectList } from './components/project-list.js';
-
-// create a new instance of the class to render the form to the DOM when the app loads
 new ProjectInput();
-
 new ProjectList('active');
-
 new ProjectList('finished');
+//# sourceMappingURL=app.js.map
